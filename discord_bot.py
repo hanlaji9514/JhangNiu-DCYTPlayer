f486fb4d80cb9d78700abb4cd93b3eeb290dad33
marsh-fish <zxcvb600870024@gmail.com>
2025-07-13 23:33:26 +0800

    Revise the miss of play command

--- discord_bot.py
+++ discord_bot.py
@@ -381,7 +381,10 @@ async def play(ctx, *, search: str = None):
     }
     
     await music_engine.queue.put(request)
-    await ctx.send(f"✅ 已將 `{song_title}` 加入到待播清單！")
+    if song_title is None:
+        await ctx.send(f"✅ 已將 `{search}` 加入到待播清單！")
+    else:
+        await ctx.send(f"✅ 已將 `{song_title}` 加入到待播清單！")
 
 @bot.command(name='skip', help='跳過目前的歌曲')
 async def skip(ctx):
